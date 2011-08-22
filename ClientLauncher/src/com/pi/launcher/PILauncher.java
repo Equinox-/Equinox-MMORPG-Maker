42d056d75a3ef20019030bafe5defdd355020ae9
westin <westin@Mach-Server>
2011-08-22 05:59:38 -0700

    Added version system

--- ClientLauncher/src/com/pi/launcher/PILauncher.java
+++ ClientLauncher/src/com/pi/launcher/PILauncher.java
@@ -9,7 +9,6 @@ public class PILauncher {
     public static void main(String[] args) throws IOException {
 	PILogViewer lviewer = new PILogViewer("Launcher");
 	PILogger lgr = new PILogger(lviewer.logOut);
-	Binaries.loadBinaries(lgr);
-	Natives.load(lgr);
+	Updater.update(lgr);
     }
 }
